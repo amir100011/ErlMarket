c9e54e0386b4132de91aaf69442968ee2cfefacf
Dor Livne <dorlivne2308@gmail.com>
2019-07-24 15:57:00 +0300

    fixed bugs and added an initial GUI
    1) fixed termination bugs where we didn't terminate the gen_servers correctly(see handle case of terminate)
    2) added compiler for fast compilation
    3) added interface which is our GUI environment, this is the first commit it holds a counter of customers and a start button to the masterFunction

---
+++ src/compiler.erl
@@ -0,0 +1,21 @@
+%%%-------------------------------------------------------------------
+%%% @author dorliv
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Jul 2019 4:35 PM
+%%%-------------------------------------------------------------------
+-module(compiler).
+-author("dorliv").
+
+%% API
+-export([compiler/0]).
+
+compiler()->
+  io:fwrite("~p~n", [compile:file(inventory, debug_info)]),
+  io:fwrite("~p~n", [compile:file(cashierServer, debug_info)]),
+  io:fwrite("~p~n", [compile:file(purchaseDepartment, debug_info)]),
+  io:fwrite("~p~n", [compile:file(masterFunction, debug_info)]),
+  io:fwrite("~p~n", [compile:file(customer, debug_info)]),
+  io:fwrite("~p~n", [compile:file(department, debug_info)]).
