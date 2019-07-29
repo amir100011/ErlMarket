c5ad8fac8068852f0f08090dfe01717eea110de9
Dor Livne <dorlivne2308@gmail.com>
2019-07-29 23:45:26 +0300

    added histogram feature to GUI
    1) added histogram feature to GUI
    2) added python script that handles the Histogram plot
    3) in order to use this new feature one must change the path of the script at the init of the interface gen server --- this will explode before changing it !!!!

---
+++ src/drawHistogram.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+from erlport.erlterms import List
+from collections import defaultdict
+from multiprocessing import Process, Queue
+QueueErlang = Queue()  # communication channel between erlang and the plot process
+Products = {'dairy': ['cheese', 'milk', 'yogurt'],
+            'meat': ['steak', 'chicken'],
+            'bakery': ['bread', 'buns']}
+
+
+def processDepartmentDataFromErlang(title: List, productList: List):
+    departmentName = title.to_string()
+    defaultProductsList = Products[departmentName]
+    dictAmountPerProduct = defaultdict(int)
+    for item in defaultProductsList:
+        dictAmountPerProduct[item] = 0
+    for item in productList:
+        nameOfProduct = item[2]
+        amount = item[-1]
+        nameOfProduct = nameOfProduct.to_string()
+        dictAmountPerProduct[nameOfProduct] += amount
+    messageToPlotProcess = ["histogram", departmentName, dictAmountPerProduct]
+    QueueErlang.put(messageToPlotProcess)
+
+
+def drawHistogramHelper(QueueData:Queue):
+    title = QueueData.get()
+    data = QueueData.get()
+    plt.figure(num=1)
+    ProductNames = data.keys()
+    amountPerProduct = []
+    for item in ProductNames:
+        amountPerProduct.append(data[item])
+    ax = plt.subplot(111)
+    width = 0.3
+    bins = list(map(lambda x: x, range(1, len(ProductNames) + 1)))
+    plt.bar(bins, amountPerProduct, width=width)
+    plt.title(title)
+    ax.set_xticks(bins)
+    ax.set_xticklabels(ProductNames)
+    plt.show()  # this blocks the process , that is why we did all this mess
+
+
+def plotProcessStart():
+    plotProcess = Process(target=plotProcessLoop, args=())
+    plotProcess.start()
+
+
+def plotProcessStop():
+    QueueErlang.put(["terminate"])
+
+
+def plotProcessLoop(drawHistogramProcess=None):
+    item = QueueErlang.get()
+    if item[0] == "histogram":
+            [_, title, dataDict] = item
+            if drawHistogramProcess is not None:
+                drawHistogramProcess.terminate()  # it can only be a living process from a previous iteration
+            QueueData = Queue()
+            drawHistogramProcess = Process(target=drawHistogramHelper, args=(QueueData,))
+            drawHistogramProcess.start()
+            QueueData.put(title)
+            QueueData.put(dataDict)
+            plotProcessLoop(drawHistogramProcess)  # recursive call for further instructions
+    if item[0] == "terminate":
+        print("I got to terminate")
+        if drawHistogramProcess is not None:
+                drawHistogramProcess.terminate()  # it can only be a living process from a previous iteration
+
+"""
+if __name__ == '__main__':
+    title = "test"
+    Productdict = defaultdict(int)
+    Productdict["milk"] = 5
+    Productdict["yogurt"] = 10
+    Productdict["dasd"] = 0
+    plotProcessStart()
+    QueueErlang.put(["histogram", title, Productdict])
+    i = 0
+    while i < 300000:
+        print("im still alive {}".format(i))
+        i = i + 1
+    title = "test2"
+    Productdict = defaultdict(int)
+    Productdict["milk"] = 30
+    Productdict["yogurt"] = 15
+    Productdict["dasd"] = 20
+    QueueErlang.put(["histogram", title, Productdict])
+    i = 0
+    while i < 300000:
+        print("im still alive {}".format(i))
+        i = i + 1
+    plotProcessStop()
+"""
+
--- src/interface.erl
+++ src/interface.erl
@@ -17,37 +17,52 @@
 -export([start/0, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2, init/1]).
 -define(STARTBUTTON, 1).
 -define(COUNTERBOX,2).
--record(state, {counter, button, start}).
+-define(DRAWBUTTON, 3).
+-define(INTERVALBETWEENPLOTS, 2500).  % should be long because doc warn not to over use the python instance to avoid OS take over
+-record(state, {counter, button, start, histogramProcess, histogram, histogramButton}).
 
 
 start()->
   gen_server:start({global, ?MODULE}, ?MODULE, [], []).
 
 init([])->
-    Wx = wx:new(),
-    Frame = wxFrame:new(Wx, ?wxID_ANY, "ErlMarket",[{size, ?SIZE}]),
-    Panel = wxPanel:new(Frame),
-    Counter = wxTextCtrl:new(Frame, ?COUNTERBOX, [{value, "0"}]),
-    Label = wxStaticText:new(Frame, ?wxID_ANY, "Number of Customers:"),
-    Font = wxFont:new(16, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?wxFONTWEIGHT_BOLD),
-    wxTextCtrl:setFont(Counter, Font),
-    StartButton = wxButton:new(Frame, ?STARTBUTTON, [{label, "Start"}]),
-    CounterSizer = wxBoxSizer:new(?wxHORIZONTAL),
-    wxSizer:add(CounterSizer, Label, [{flag, ?wxALL bor ?wxALIGN_CENTRE},
-      {border, 5}]),
-    wxSizer:add(CounterSizer, Counter, [{proportion,1}, {flag, ?wxEXPAND bor ?
-    wxALL}, {border, 5}]),
-    MainSizer = wxBoxSizer:new(?wxVERTICAL),
-    wxSizer:add(MainSizer, CounterSizer, [{flag, ?wxEXPAND}]),
-    wxSizer:add(MainSizer, StartButton, [{flag, ?wxEXPAND bor ?wxALL}, {border,
+      Wx = wx:new(),
+      Frame = wxFrame:new(Wx, ?wxID_ANY, "ErlMarket",[{size, ?SIZE}]),
+      Counter = wxTextCtrl:new(Frame, ?COUNTERBOX, [{value, "0"}]),
+      Label = wxStaticText:new(Frame, ?wxID_ANY, "Number of Customers:"),
+      Font = wxFont:new(16, ?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?wxFONTWEIGHT_BOLD),
+      wxTextCtrl:setFont(Counter, Font),
+      StartButton = wxButton:new(Frame, ?STARTBUTTON, [{label, "Start"}]),
+      DepartmentScrollButton = wxComboBox:new(Frame, ?wxID_ANY, [{choices,["dairy","meat","bakery"]}]),
+      wxComboBox:setValue(DepartmentScrollButton, "dairy"),
+      put(department, dairy),
+      DrawHistogramOfDepartmentProduct = wxButton:new(Frame, ?DRAWBUTTON, [{label, "Draw Histogram"}]),
+
+      CounterSizer = wxBoxSizer:new(?wxHORIZONTAL),
+      wxSizer:add(CounterSizer, Label, [{flag, ?wxALL bor ?wxALIGN_CENTRE},
+        {border, 5}]),
+      wxSizer:add(CounterSizer, Counter, [{proportion,1}, {flag, ?wxEXPAND bor ?
+      wxALL}, {border, 5}]),
+      wxSizer:add(CounterSizer, DrawHistogramOfDepartmentProduct, [{proportion,1}, {flag, ?wxEXPAND bor ?
+      wxALL}, {border, 5}]),
+      MainSizer = wxBoxSizer:new(?wxVERTICAL),
+      wxSizer:add(MainSizer, CounterSizer, [{flag, ?wxEXPAND}]),
+      wxSizer:add(MainSizer, StartButton, [{flag, ?wxEXPAND bor ?wxALL}, {border,
+        5}]),
+      wxSizer:add(MainSizer, DepartmentScrollButton, [{flag, ?wxEXPAND bor ?wxALL}, {border,
       5}]),
-    wxWindow:setSizer(Frame, MainSizer),
-    wxSizer:setSizeHints(MainSizer, Frame),
-    wxWindow:setMinSize(Frame, wxWindow:getSize(Frame)),
-    wxButton:connect(StartButton, command_button_clicked),
-    wxFrame:connect(Frame, close_window),
-    wxFrame:show(Frame),
-    {ok, #state{counter = Counter, button = StartButton, start=false}}.
+      wxWindow:setSizer(Frame, MainSizer),
+      wxSizer:setSizeHints(MainSizer, Frame),
+      wxWindow:setMinSize(Frame, wxWindow:getSize(Frame)),
+
+      wxButton:connect(StartButton, command_button_clicked),
+      wxComboBox:connect(DepartmentScrollButton, command_combobox_selected),
+      wxButton:connect(DrawHistogramOfDepartmentProduct, command_button_clicked),
+      wxFrame:connect(Frame, close_window),
+      wxFrame:show(Frame),
+      {ok, P} = python:start([{python_path, "/home/dorliv/Desktop/Erlang/histogram"},{python, "python3"}]), % initialize a python instance
+      {ok, #state{counter = Counter, button = StartButton, start = false,
+        histogramProcess = P, histogram = false, histogramButton = DrawHistogramOfDepartmentProduct}}.
 
 
 handle_call(_Request, _From, State) ->
@@ -59,7 +74,14 @@ handle_cast(_Msg, State) ->
 
 
 
-handle_info(#wx{obj = Button, event = #wxCommand{type = command_button_clicked}}, #state{counter = Counter, start = false} = State) ->
+
+handle_info(#wx{obj = DepartmentScrollButton, event = #wxCommand{type = command_combobox_selected}}, State) ->
+  Value = wxComboBox:getValue(DepartmentScrollButton),
+  put(department, list_to_atom(Value)),
+  {noreply, State};
+
+
+handle_info(#wx{id = ?STARTBUTTON , obj = Button, event = #wxCommand{type = command_button_clicked}}, #state{counter = Counter, start = false} = State) ->
     masterFunction:start(),
     wxTextCtrl:setEditable(Counter, false),
     wxButton:setLabel(Button, "Stop"),
@@ -67,9 +89,29 @@ handle_info(#wx{obj = Button, event = #wxCommand{type = command_button_clicked}}
     {noreply, State#state{start = true}};
 
 %%  @doc pressing stop closes the shop
-handle_info(#wx{obj = _, event = #wxCommand{type = command_button_clicked}}, #state{counter = _, start = true} = State) ->
+handle_info(#wx{id = ?STARTBUTTON, event = #wxCommand{type = command_button_clicked}}, #state{start = true} = State) ->
   masterFunction:castFunc(closeShop),
   {noreply, State#state{start = false}};
+
+
+handle_info(#wx{id = ?DRAWBUTTON ,obj = Button, event = #wxCommand{type = command_button_clicked}},
+    #state{start = true, histogramProcess = P, histogram = false} = State) ->
+  python:call(P, drawHistogram, plotProcessStart, []), % initialize the process that handles the plots
+  erlang:send_after(1000, self(), plotHistogram),
+  wxButton:setLabel(Button, "Stop Drawing Histogram"),
+  {noreply, State#state{histogram = true}};
+
+handle_info(#wx{id = ?DRAWBUTTON ,obj = Button, event = #wxCommand{type = command_button_clicked}},
+    #state{start = true, histogramProcess = P, histogram = true} = State) ->
+  python:call(P, drawHistogram, plotProcessStop, []), % closes plotProcess
+  wxButton:setLabel(Button, "Draw Histogram"),
+  {noreply, State#state{histogram = false}};
+
+handle_info(#wx{id = ?DRAWBUTTON, event = #wxCommand{type = command_button_clicked}}, #state{ start = false} = State) ->
+  io:fwrite("ERROR : Draw Histogram was pressed before system start~n"),
+  {noreply, State};
+
+
 
 
 handle_info(updateCounter, #state{counter = Counter, start = true} = State) ->
@@ -78,7 +120,22 @@ handle_info(updateCounter, #state{counter = Counter, start = true} = State) ->
     erlang:send_after(1000, self(), updateCounter),  % update every second
     {noreply, State};
 
-handle_info(updateCounter, #state{button = Button, counter = Counter, start = false} = State) ->
+
+handle_info(updateCounter, #state{button = Button, counter = Counter, start = false, histogram = false} = State) ->
+  try masterFunction:callFunc(getNumberOfCustomers) of
+    N when is_integer(N) -> wxTextCtrl:setValue(Counter, integer_to_list(N)),
+      erlang:send_after(1000, self(), updateCounter),  % update every second
+      {noreply, State}
+  catch
+    exit:_ -> wxTextCtrl:setValue(Counter, "0"),
+             wxTextCtrl:setEditable(Counter, false),
+              wxButton:setLabel(Button, "Start"),
+            {noreply, State};
+    Throw -> exit(Throw)
+  end;
+
+handle_info(updateCounter, #state{button = Button, counter = Counter, start = false,
+  histogramButton = HistogramButton, histogramProcess = P , histogram = true} = State) ->
   try masterFunction:callFunc(getNumberOfCustomers) of
     N when is_integer(N) -> wxTextCtrl:setValue(Counter, integer_to_list(N)),
                             erlang:send_after(1000, self(), updateCounter),  % update every second
@@ -86,27 +143,52 @@ handle_info(updateCounter, #state{button = Button, counter = Counter, start = fa
   catch
     exit:_ -> wxTextCtrl:setValue(Counter, "0"),
                    wxTextCtrl:setEditable(Counter, false),
+                   python:call(P, drawHistogram, plotProcessStop, []), % closes plotProcess
                    wxButton:setLabel(Button, "Start"),
-                   {noreply, State};
+                   wxButton:setLabel(HistogramButton, "Draw Histogram"),
+                   {noreply, State#state{histogram = false}};
     Throw -> exit(Throw)
   end;
-%%  case NumberOfCustomers of
-%%    0 -> wxTextCtrl:setValue(Counter, "0"),
-%%         wxTextCtrl:setEditable(Counter, false),
-%%         wxButton:setLabel(Button, "Start"),
-%%         {noreply, State};
-%%    N ->   wxTextCtrl:setValue(Counter, integer_to_list(N)),
-%%           erlang:send_after(1000, self(), updateCounter),  % update every second
-%%           {noreply, State}
-%%  end;
 
 
-handle_info(#wx{event=#wxClose{}}, State) ->
+
+handle_info(plotHistogram, #state{histogram = true , histogramProcess = P} = State) ->
+  DepartmentName = get(department),
+  try department:callFunc(DepartmentName, getProducts) of  % if the department gets turned off before the histogram status was changed
+    ProductFromDepartment ->  python:call(P, drawHistogram, processDepartmentDataFromErlang, [atom_to_list(DepartmentName), ProductFromDepartment]),
+                              erlang:send_after(?INTERVALBETWEENPLOTS, self(), plotHistogram),  % update plot every interval
+                              {noreply, State}
+  catch
+    exit:_ ->
+               {noreply, State}
+                %  we do not change the state in this case
+                % this is intentional because we want to get to update Counter where we close the drawHistogram in an orderly fashion
+  end;
+
+
+
+handle_info(plotHistogram, #state{histogram = false} = State) -> % stop sending message to self after pressing stop drawing histogram
+  {noreply, State};
+
+
+handle_info(#wx{event=#wxClose{}}, #state{start = true} = State) ->
+  masterFunction:castFunc(closeShop),
+  io:fwrite("Interface says bye bye ~n"),
+  {stop, normal, State#state{histogram = false}};
+
+handle_info(#wx{event=#wxClose{}}, #state{start = false} = State) ->
   io:fwrite("Interface says bye bye ~n"),
   {stop, normal, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, #state{histogramProcess = P, histogram = true} = State) ->
   wx:destroy(),
+  python:call(P, drawHistogram, plotProcessStop, []), % closes plotProcess
+  python:stop(P), % terminating python instance
+  ok;
+
+terminate(_Reason, #state{histogramProcess = P, histogram = false} = State) ->
+  wx:destroy(),
+  python:stop(P), % terminating python instance
   ok.
 
 
