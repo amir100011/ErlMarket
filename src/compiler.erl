657955962d25ecd216333640735b42aa54091b55
amir <amirts@post.bgu.ac.il>
2019-07-30 15:25:20 +0300

    Changes: 1) added watch_dog script 2) interface is now watch dog protected

--- src/compiler.erl
+++ src/compiler.erl
@@ -11,6 +11,7 @@
 
 %% API
 -export([compiler/0]).
+-compile(export_all).
 
 compiler()->
   io:fwrite("~p~n", [compile:file(inventory, debug_info)]),
@@ -22,4 +23,16 @@ compiler()->
   io:fwrite("~p~n", [compile:file(interface, debug_info)]),
   inventory:initInventory(node()),
   interface:start().
-  %masterFunction:start().
+
+connect() ->
+  net_kernel:connect_node('server@amir-Inspiron-5559'),
+  net_kernel:connect_node('server2@amir-Inspiron-5559'),
+  net_kernel:connect_node('watchdog2@amir-Inspiron-5559'),
+  io:fwrite("~p~n", [compile:file(server, debug_info)]),
+  io:fwrite("~p~n", [compile:file(watchdog, debug_info)]),
+  watchdog:init().
+
+compileTest() ->
+  io:fwrite("~p~n", [compile:file(interface, debug_info)]),
+  io:fwrite("~p~n", [compile:file(watchdog, debug_info)]),
+  watchdog:init(interface).
