755666ab91242921c6899843feb2ff2cadd93bba
Dor Livne <dorlivne2308@gmail.com>
2019-08-04 20:13:23 +0300

    Nodes for inter-machine comm

--- src/records.hrl
+++ src/records.hrl
@@ -13,9 +13,9 @@
 -record(shoppinlistelement, {department_name, product_name, price, amount}).
 -record(processesAllocationToNodes,{monitorRef,moduleName,processName}).
 -define(DEPARTMENT_LIST, [dairy, meat, bakery]).
--define(PURCHASE_DEPARTMENT_NODE, 'dor@L108W001').
--define(CASHIER_SERVER_NODE,  'amir@L108W001').
--define(MASTER_SERVER_NODE,  'master@L108W001').
--define(INTERFACE_NODE,  'interface@L108W001').
--define(NodeList, [?PURCHASE_DEPARTMENT_NODE, ?INTERFACE_NODE, ?MASTER_SERVER_NODE, ?CASHIER_SERVER_NODE ]).%, ?CASHIER_SERVER_NODE, ?MASTER_SERVER_NODE, ?INTERFACE_NODE]).
+-define(PURCHASE_DEPARTMENT_NODE, 'amir@132.72.52.249').
+-define(CASHIER_SERVER_NODE,  amir@132.72.52.249'').
+-define(MASTER_SERVER_NODE,  'dor@132.72.54.107').
+-define(INTERFACE_NODE,  'dor@132.72.54.107').
+-define(NodeList, [?INTERFACE_NODE, ?CASHIER_SERVER_NODE ]).%, ?CASHIER_SERVER_NODE, ?MASTER_SERVER_NODE, ?INTERFACE_NODE]).
 -include_lib("stdlib/include/qlc.hrl").
